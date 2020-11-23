cadf6be2cda9d65541bd6b757a6b48726d1445d5
Akshay <36558161+pjanicked@users.noreply.github.com>
2020-11-23 23:22:48 +0530

    Add environment and templates (#1)
    * added templates for stage
    
    * fixed yaml syntax
    
    * changed stages to jobs
    
    * fixed job syntax
    
    * changed action.yaml to steps
    
    * stage name renamed
    
    * reverted shell to one address param, added dependson job
    
    * changed sync pipeline and variable
    
    * changed kong.yaml for kong version 1
    
    * fixed whitespace in shellscript

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,24 +5,21 @@ main (){
     cmd=$1
     dir=$2
     ops=$3
-	addr1=$4
-	addr2=$5
+    opsvalue=$4
     if [ ! -e ${dir} ]; then
         echo "${dir}: No such file or directoy exists";
         exit 1;
     fi
 
     for file in $(ls ${dir}); do
-        echo "Executing: deck $cmd --$ops $addr1 -s $dir/$file"
-        deck $cmd --$ops $addr1 -s $dir/$file
-		echo "Executing: deck $cmd --$ops $addr2 -s $dir/$file"
-        deck $cmd --$ops $addr2 -s $dir/$file
+        echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"
+        deck $cmd --$ops $opsvalue -s $dir/$file
     done
 }
 
 
 case $1 in
-    "ping") deck $1 --$3 $4; deck $1 --$3 $5;;
-    "validate"|"diff"|"sync") main $1 $2 "$3" "$4" "$5";;
+    "ping") deck $1 --$3 $4;;
+    "validate"|"diff"|"sync") main $1 $2 "$3" "$4";;
     * ) echo "deck $1 is not supported." && exit 1 ;;
 esac
