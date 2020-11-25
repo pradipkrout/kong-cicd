09c53d19634160843a10deffd3abef2d8085dea7
pjanicked <aekkim.13@gmail.com>
2020-11-25 11:36:24 +0530

    multiple kong yaml files added and changed shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,24 @@ main (){
     dir=$2
     ops=$3
     opsvalue=$4
+	env=$5
     if [ ! -e ${dir} ]; then
         echo "${dir}: No such file or directoy exists";
         exit 1;
     fi
 
     for file in $(ls ${dir}); do
-        echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"
-        deck $cmd --$ops $opsvalue -s $dir/$file
+		case $env in
+			"test") echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"; deck $cmd --$ops $opsvalue -s $dir/$file;;
+			"master") echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"; deck $cmd --$ops $opsvalue -s $dir/$file;;
+			* ) echo "No files exist" && exit 1 ;;
+		esac        
     done
 }
 
 
 case $1 in
     "ping") deck $1 --$3 $4;;
-    "validate"|"diff"|"sync") main $1 $2 "$3" "$4";;
+    "validate"|"diff"|"sync") main $1 $2 "$3" "$4" "$5";;
     * ) echo "deck $1 is not supported." && exit 1 ;;
 esac
