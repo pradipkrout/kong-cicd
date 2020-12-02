40f7411193ca16ecc2ea4fb66c7464179f8e0e55
pjanicked <aekkim.13@gmail.com>
2020-12-02 23:18:11 +0530

    changed yaml and shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,14 @@ main (){
     dir=$2
     ops=$3
     opsvalue=$4
-	env=$5
+	kongfile=$5
     if [ ! -e ${dir} ]; then
         echo "${dir}: No such file or directoy exists";
         exit 1;
     fi
 
-    for file in $(ls ${dir}); do
-		case $env in
-			"test") echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"; deck $cmd --$ops $opsvalue -s $dir/$file;;
-			"master") echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$file"; deck $cmd --$ops $opsvalue -s $dir/$file;;
-			* ) echo "No files exist" && exit 1 ;;
-		esac        
-    done
+    echo "Executing: deck $cmd --$ops $opsvalue -s $dir/$kongfile" 
+	deck $cmd --$ops $opsvalue -s $dir/$kongfile
 }
 
 
